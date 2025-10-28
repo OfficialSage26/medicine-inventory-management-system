dd6271c9623a334e667c0574b9650fc0465ef0f2
Sagee <johnashgames23@gmail.com>
2025-10-28 19:20:59 +0800

    fix: redirect to relative path in delete.php

--- medicines/delete.php
+++ medicines/delete.php
@@ -3,11 +3,11 @@ require_once __DIR__ . '/../helpers/auth_helper.php';
 requireLogin();
 require_once __DIR__ . '/../config/db.php';
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    redirect('/medicines/index.php');
+    redirect('medicines/index.php');
 }
 if (!verify_csrf_token($_POST['csrf_token'] ?? null)) {
     $_SESSION['flash'] = ['type' => 'danger', 'message' => 'Invalid request. Please try again.'];
-    redirect('/medicines/index.php');
+    redirect('medicines/index.php');
 }
 $id = isset($_POST['id']) ? (int)$_POST['id'] : 0;
 try {
